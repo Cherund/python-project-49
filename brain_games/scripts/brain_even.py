bd79f3ed21c15cd44f791a7661cbed686b1a3893
Egor Gerimovich <cherund@gmail.com>
2023-11-15 12:44:36 +0100

    created brain_even.py and implemented even game, modifed cli.py to return name of a player

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,33 @@
+from random import randint
+from prompt import string
+from brain_games.cli import welcome_user
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = welcome_user()
+    print("Answer 'yes' if the number is even, otherwise answer 'no'.")
+    result = 0
+    while result < 3:
+        number = randint(3, 99)
+        correct_answer = number % 2
+        print(f'Question: {number}')
+        player_answer = string('Your answer: ')
+        if correct_answer:
+            correct_answer = 'no'
+        else:
+            correct_answer = 'yes'
+
+        if player_answer.lower() == correct_answer:
+            print('Correct!')
+            result += 1
+        else:
+            print(f"'{player_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.\n"
+                  f"Let's try again, {name}!")
+            result = 0
+
+    print(f'Congratulations, {name}!')
+
+
+if __name__ == '__main__':
+    main()
