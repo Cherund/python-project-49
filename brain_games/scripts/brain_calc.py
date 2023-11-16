d6611a729b636b932b09f04c463d6826fb67ff9c
Egor Gerimovich <cherund@gmail.com>
2023-11-16 17:12:57 +0100

    all logic placed inside game dir

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,34 +1,8 @@
-from random import randint, choice
-from prompt import string
-from brain_games.cli import welcome_user, game_end
-import operator
-
-ops = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-}
+from brain_games.games.calc import calc
 
 
 def main():
-    name = welcome_user()
-    print("What is the result of the expression?")
-    result = 0
-    while result < 3:
-        number1 = randint(2, 99)
-        number2 = randint(2, 99)
-        operation = choice(['*', '+', '-'])
-        correct_answer = ops[operation](number1, number2)
-
-        print(f'Question: {number1} {operation} {number2}')
-        player_answer = string('Your answer: ')
-        if player_answer.strip() == str(correct_answer):
-            print('Correct!')
-            result += 1
-        else:
-            break
-
-    game_end(player_answer, correct_answer, result, name)
+    calc()
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,29 +1,8 @@
-from random import randint
-from prompt import string
-from brain_games.cli import welcome_user, game_end
+from brain_games.games.even import even
 
 
 def main():
-    name = welcome_user()
-    print("Answer 'yes' if the number is even, otherwise answer 'no'.")
-    result = 0
-    while result < 3:
-        number = randint(3, 99)
-        correct_answer = number % 2
-        print(f'Question: {number}')
-        player_answer = string('Your answer: ')
-        if correct_answer:
-            correct_answer = 'no'
-        else:
-            correct_answer = 'yes'
-
-        if player_answer.lower() == correct_answer:
-            print('Correct!')
-            result += 1
-        else:
-            break
-
-    game_end(player_answer, correct_answer, result, name)
+    even()
 
 
 if __name__ == '__main__':
